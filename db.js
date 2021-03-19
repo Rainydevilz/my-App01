c7f0ee155ba0970585882786bdce49d76b9f2793
Nattakorn <nonmom2550@gmail.com>
2021-03-19 17:51:37 +0700

    add uri mongodb

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const passportLocal = 'mongodb://localhost:27017/passport'
 const fishCould = 'mongodb+srv://fishmin:fishmin@1234@cluster0.qavck.mongodb.net/passport?retryWrites=true&w=majority'
+const MONGO_DB_URI = process.env.MONGODB_URI;
 mongoose.connect(fishCould, {
   useNewUrlParser: true,
   useUnifiedTopology: true
